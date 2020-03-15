da84ec9cb79eefc2c2f683505de1781645245654
vignesh1226 <61319256+vignesh1226@users.noreply.github.com>
2020-03-15 14:22:14 +0530

    projectc28

---
+++ Slingshot.js
@@ -0,0 +1,26 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.Sling = Constraint.create(options);
+        World.add(world, this.Sling);
+    }
+
+    display(){
+        if(this.Sling.bodyA){
+        var pointA = this.Sling.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        this.Sling.bodyA = null;
+    }
+    
+}
